8d408299f6bcb63cfcff0e48966b470d9b1127ac
Aleksey Usatov <usatov.aleksey@gmail.com>
2015-11-07 23:28:48 +0300

    Fix the solution for the exercise 1.17 and add a solution for the exercise 1.18

--- src/ch01/1-17.rkt
+++ src/ch01/1-17.rkt
@@ -1,18 +1,15 @@
 #lang racket
 
-(define (* base n)
-  (define (double n)
+(define (double n)
     (+ n n))
-  (define (halve n)
+(define (halve n)
     (/ n 2))
-  (define (even? n)
+(define (even? n)
     (= (remainder n 2) 0))
-  (define (iter base counter product)
-    (cond ((= counter 0) product)
-          ((even? counter) (iter (double base)
-                                 (halve counter)
-                                 product))
-          (else (iter base
-                      (- counter 1)
-                      (+ product base)))))
-  (iter base n 0))
+
+(define (fast-mult a b)
+  (cond ((= b 0) 0)
+        ((even? b) (fast-mult (double a)
+                              (halve b)))
+        (else (+ a
+                 (fast-mult a (- b 1))))))
